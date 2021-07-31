2b74062d16d480961bce831bb1e385ec61f37610
Mustafa <mustafa.katha@gmail.com>
2021-08-01 02:39:09 +1000

    updated main

--- main.py
+++ main.py
@@ -27,7 +27,26 @@ class PersonInfo(BaseModel):
     capital_loss: int = Field(None, alias="capital-loss")
     hours_per_week: int = Field(None, alias="hours-per-week")
     native_country: str = Field(None, alias="native-country")
-
+    
+    class Config:
+        schema_extra = {
+            "example": {
+                "age": 39,
+                "workclass": "State-gov",
+                "fnlgt": 77516,
+                "education": "Bachelors",
+                "education-_num": 13,
+                "marital_status": "Never-married",
+                "occupation": "Adm-clerical",
+                "relationship": "Not-in-family",
+                "race": "White",
+                "sex": "Male",
+                "capital_gain": 2174,
+                "capital_loss": 0,
+                "hours_per_week": 40,
+                "native_country": "United-States",
+            }
+        }
 
 app = FastAPI()
 
